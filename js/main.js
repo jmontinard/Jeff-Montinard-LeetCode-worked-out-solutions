1dac01b40c81a92c5229a0e55c5eab26a7139e91
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-08-11 23:36:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,36 +24,52 @@
      
      const indices = []
 
-     if(nums.length === 2 && nums[0] + nums[1] === target ){
-        indices.push(0,1)
-    }
+//   const gotcha = nums.filter((num,i)=>{
+//       let held = i
+
+//       if(   num + nums[i] === target){
+//           console.log(num,nums[i])
+//       }
+//   })
+//   console.log(gotcha)
+
+    //  if(nums.length === 2 && nums[0] + nums[1] === target ){
+    //     indices.push(0,1)
+    // }
 
      //  we want to return indices of the arr that are the sum of the target 
      
      // one loop through the arr and store ar[0] to a var and test that against the other indices 
-     for (let i = 0; i < nums.length; i++){
-     //we hold this to the actual value bc if hold it to nums[i] our 1st part of the if statment will stop the rest from running 
-     let holderInt = nums[0]
+    //  for (let i = 0; i < nums.length; i++){
+    //  //we hold this to the actual value bc if hold it to nums[i] our 1st part of the if statment will stop the rest from running 
+    //  let holderInt = nums[0]
 
-    // so this checks arr0 against the arr[i] to see if its not a match so that way we dont get the same numbers 
-    // then we check that heldNum against the other indices to see if they sum up to the target so if they do we push the indexs of both in our rtn arr
+    // // so this checks arr0 against the arr[i] to see if its not a match so that way we dont get the same numbers 
+    // // then we check that heldNum against the other indices to see if they sum up to the target so if they do we push the indexs of both in our rtn arr
+
+     
+
+
+
    
-    if(holderInt != nums[i] && holderInt + nums[i] === target){
-    console.log(nums[i])
-        indices.push(nums.indexOf(holderInt), nums.indexOf(nums[i]))
-    } 
-    // now if that held number is not the it at the end of that loop change the heldNum to the next index and repeat 
-    else if (holderInt != nums[i] && holderInt + nums[i] != target){
-    holderInt = nums[0 + 1]
-    }
-    console.log(holderInt)
+    // if(holderInt != nums[i] && holderInt + nums[i] === target){
+    // console.log(nums[i])
+    //     indices.push(nums.indexOf(holderInt), nums.indexOf(nums[i]))
+    // } 
+    // // now if that held number is not the it at the end of that loop change the heldNum to the next index and repeat 
+    // else if ( holderInt != nums[i] && holderInt + nums[i] != target){
+    // holderInt = nums[i + 1]
+    // console.log(holderInt)
+    // }
 
-    }
-    console.log(indices)
-        // if it and any of the next sums up to the target  push that into a empty arr
+    // }
+    // console.log(indices)
+    //     // if it and any of the next sums up to the target  push that into a empty arr
         
     };
 
 //  twoSum([2,7,11,15], 9)
  twoSum([3,2,4], 6)
 // twoSum([3,3], 6)
+
+//  holderInt != nums[i] &&
