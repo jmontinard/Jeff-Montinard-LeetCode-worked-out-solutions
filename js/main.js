9ec16ced8b6699f5e69d3dd4f6e0d2efee75dafa
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-09-21 13:15:44 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -843,3 +843,223 @@ console.log(conga.elementAt(3));
 console.log(conga.indexOf('Puppy'));
 console.log(conga.size());
 console.log(conga.size());
+
+
+
+
+
+/* Trie Data Structure */
+
+let Node = function() {
+	this.keys = new Map();
+	this.end = false;
+	this.setEnd = function() {
+		this.end = true;
+	};
+	this.isEnd = function() {
+		return this.end;
+	};
+};
+
+let Trie = function() {
+
+	this.root = new Node();
+
+	this.add = function(input, node = this.root) {
+		if (input.length == 0) {
+			node.setEnd();
+			return;
+		} else if (!node.keys.has(input[0])) {
+			node.keys.set(input[0], new Node());
+			return this.add(input.substr(1), node.keys.get(input[0]));
+		} else {
+			return this.add(input.substr(1), node.keys.get(input[0]));
+		};
+	};
+
+	this.isWord = function(word) {
+		let node = this.root;
+		while (word.length > 1) {
+			if (!node.keys.has(word[0])) {
+				return false;
+			} else {
+				node = node.keys.get(word[0]);
+				word = word.substr(1);
+			};
+		};
+		return (node.keys.has(word) && node.keys.get(word).isEnd()) ? 
+      true : false;
+	};
+
+	this.print = function() {
+		let words = new Array();
+		let search = function(node, string) {
+			if (node.keys.size != 0) {
+				for (let letter of node.keys.keys()) {
+					search(node.keys.get(letter), string.concat(letter));
+				};
+				if (node.isEnd()) {
+					words.push(string);
+				};
+			} else {
+				string.length > 0 ? words.push(string) : undefined;
+				return;
+			};
+		};
+		search(this.root, new String());
+		return words.length > 0 ? words : mo;
+	};
+
+};
+
+myTrie = new Trie()
+myTrie.add('ball'); 
+myTrie.add('bat'); 
+myTrie.add('doll'); 
+myTrie.add('dork'); 
+myTrie.add('do'); 
+myTrie.add('dorm')
+myTrie.add('send')
+myTrie.add('sense')
+console.log(myTrie.isWord('doll'))
+console.log(myTrie.isWord('dor'))
+console.log(myTrie.isWord('dorf'))
+console.log(myTrie.print())
+
+
+
+
+
+/* Heaps */
+
+// left child: i * 2
+// right child: i * 2 + 1
+// parent: i / 2
+
+let MinHeap = function() {
+	
+	let heap = [null];
+	
+	this.insert = function(num) {
+		heap.push(num);
+		if (heap.length > 2) {
+			let idx = heap.length - 1;
+			while (heap[idx] < heap[Math.floor(idx/2)]) {
+				if (idx >= 1) {
+					[heap[Math.floor(idx/2)], heap[idx]] = [heap[idx], heap[Math.floor(idx/2)]];
+					if (Math.floor(idx/2) > 1) {
+						idx = Math.floor(idx/2);
+					} else {
+						break;
+					};
+				};
+			};
+		};
+	};
+	
+	this.remove = function() {
+		let smallest = heap[1];
+		if (heap.length > 2) {
+			heap[1] = heap[heap.length - 1];
+			heap.splice(heap.length - 1);
+			if (heap.length == 3) {
+				if (heap[1] > heap[2]) {
+					[heap[1], heap[2]] = [heap[2], heap[1]];
+				};
+				return smallest;
+			};
+			let i = 1;
+			let left = 2 * i;
+			let right = 2 * i + 1;
+			while (heap[i] >= heap[left] || heap[i] >= heap[right]) {
+				if (heap[left] < heap[right]) {
+					[heap[i], heap[left]] = [heap[left], heap[i]];
+					i = 2 * i
+				} else {
+					[heap[i], heap[right]] = [heap[right], heap[i]];
+					i = 2 * i + 1;
+				};
+				left = 2 * i;
+				right = 2 * i + 1;
+				if (heap[left] == undefined || heap[right] == undefined) {
+					break;
+				};
+			};
+		} else if (heap.length == 2) {
+			heap.splice(1, 1);
+		} else {
+			return null;
+		};
+		return smallest;
+	};
+  
+	this.sort = function() {
+		let result = new Array();
+		while (heap.length > 1) {
+			result.push(this.remove());
+		};
+		return result;
+	};
+
+};
+
+let MaxHeap = function() {
+	
+	let heap = [null];
+	
+	this.print = () => heap;
+
+	this.insert = function(num) {
+		heap.push(num);
+		if (heap.length > 2) {
+			let idx = heap.length - 1;
+			while (heap[idx] > heap[Math.floor(idx/2)]) {
+				if (idx >= 1) {
+					[heap[Math.floor(idx/2)], heap[idx]] = [heap[idx], heap[Math.floor(idx/2)]];
+					if (Math.floor(idx/2) > 1) {
+						idx = Math.floor(idx/2);
+					} else {
+						break;
+					};
+				};
+			};
+		};
+	};
+	
+	this.remove = function() {
+		let smallest = heap[1];
+		if (heap.length > 2) {
+			heap[1] = heap[heap.length - 1];
+			heap.splice(heap.length - 1);
+			if (heap.length == 3) {
+				if (heap[1] < heap[2]) {
+					[heap[1], heap[2]] = [heap[2], heap[1]];
+				};
+				return smallest;
+			};
+			let i = 1;
+			let left = 2 * i;
+			let right = 2 * i + 1;
+			while (heap[i] <= heap[left] || heap[i] <= heap[right]) {
+				if (heap[left] > heap[right]) {
+					[heap[i], heap[left]] = [heap[left], heap[i]];
+					i = 2 * i
+				} else {
+					[heap[i], heap[right]] = [heap[right], heap[i]];
+					i = 2 * i + 1;
+				};
+				left = 2 * i;
+				right = 2 * i + 1;
+				if (heap[left] == undefined || heap[right] == undefined) {
+					break;
+				};
+			};
+		} else if (heap.length == 2) {
+			heap.splice(1, 1);
+		} else {
+			return null;
+		};
+		return smallest;
+	};
+
+};
