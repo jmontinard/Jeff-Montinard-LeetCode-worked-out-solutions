c7e86bbe0f02f3405b6f5ef059949e4b1239017c
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-10-06 21:06:33 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1130,6 +1130,17 @@ console.log(reverseList([1,2,3,4,5,6,7]))
 // }
 
 
+function factorial (n){
+    if(n === 1 || n === 0){
+        return 1;
+    }
+    else {
+         return n  * factorial(n - 1);
+
+    }
+
+}
+
 
 
 
