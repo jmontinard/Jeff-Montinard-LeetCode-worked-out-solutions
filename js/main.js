a5cc658ef5fba206d8db3b66e8a936b4b8a97033
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-10-09 22:31:43 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1182,18 +1182,18 @@ class Node {
 //   }
   
 //Function to sort the array using bubble sort algorithm.
-function bubbleSort(arr,n){
-    //code here
-    //bs takes a look a the first  2 elements of the array checks to see one is greater and if so swap
+// function bubbleSort(arr,n){
+//     //code here
+//     //bs takes a look a the first  2 elements of the array checks to see one is greater and if so swap
 
-    for(let i = 0; i < arr.length; i++){  
-        console.log(`i :  ${ arr[i]}`);      
-        for(let j = 0; j < arr[i].length; j++){ 
-            // console.log(arr[i][j]);
-            console.log(`j :  ${ arr[j]}`);      
+//     for(let i = 0; i < arr.length; i++){  
+//         console.log(`i :  ${ arr[i]}`);      
+//         for(let j = 0; j < arr[i].length; j++){ 
+//             // console.log(arr[i][j]);
+//             console.log(`j :  ${ arr[i][j]}`);      
 
             
-        }
+//         }
 
-    }
-   }
+//     }
+//    }
