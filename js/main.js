7b4c8aa5cb233b5cf10962b97d1c6a2076cfb416
Jeff Montinard <68262228+jmontinard@users.noreply.github.com>
2022-10-19 23:02:55 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1244,3 +1244,5 @@ function countdown(n){
   addUpTo(1000000000);
   var time2 = performance.now();
   console.log(`Time Elapsed: ${(time2 - time1) / 1000} seconds.`)
+  var time1 = performance.now();
+  addUpTo(1000000000);
